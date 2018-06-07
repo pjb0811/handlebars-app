e0977221afeaa3c12b99f8da83784d76f1442b12
jbpark <jbpark@wemakeprice.com>
2018-06-07 10:47:22 +0900

    임시 저장.

--- server/index.js
+++ server/index.js
@@ -11,10 +11,10 @@ const app = express();
 const server = http.createServer(app);
 const PORT = 9001;
 
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import config from '../webpack.dev.js';
-const compiler = webpack(config);
+// import webpack from 'webpack';
+// import webpackDevMiddleware from 'webpack-dev-middleware';
+// import config from '../webpack.dev.js';
+// const compiler = webpack(config);
 
 /* app.use(
   webpackDevMiddleware(compiler, {
@@ -34,41 +34,12 @@ staticFiles.forEach(file => {
   });
 });
 
-function normalizeAssets(assets) {
-  return Array.isArray(assets) ? assets : [assets];
-}
-
 app.get('*', async (req, res, next) => {
   const template = path.join(__dirname, '../dist/index.html');
   const beforeRenderHtml = fs.readFileSync(template).toString();
   console.log(req.url);
 
-  // const url = `${req.protocol}://${req.get('host')}${req.url}`;
-  // const { html, ttRenderMs } = await ssr({ url });
-
   res.status(200).send(beforeRenderHtml);
-
-  /*
-  let assets = res.locals.webpackStats.toJson().assetsByChunkName;
-  assets = typeof assets === 'string' ? { app: assets } : assets;
-  assets = Array.isArray(assets) ? assets : Object.values(assets);
-
-  let js = assets.filter(
-    path => String(path.split('.').pop()).toLowerCase() === 'js'
-  );
-
-  res.send(`
-  <html>
-    <head>
-      <title>Webpack - Express</title>
-    </head>
-    <body>
-      <div id="root"></div>
-      ${js.map(path => `<script src="${path}"></script>`).join('\n')}
-    </body>
-  </html>
-  `);
-  */
 });
 
 server.listen(PORT);
--- src/modules/Server.js
+++ src/modules/Server.js
@@ -1,63 +1,65 @@
+class Server {}
+export default Server;
+
 // require('jsdom-global')(`<!DOCTYPE html><body>`, {
 //   url: 'http://localhost:9001/'
 // });
 
-class Server {
-  constructor(props) {
-    this.props = props;
-    this.init();
-  }
+// class Server {
+//   constructor(props) {
+//     this.props = props;
+//     this.init();
+//   }
 
-  init() {
-    // const { el = "", routes, pathname } = this.props;
-    // if (el) {
-    //   this.rootElement = this.getRootElement({ el });
-    // }
-    // if (routes) {
-    //   this.setRoute();
-    // }
-  }
+//   init() {
+//     // const { el = "", routes, pathname } = this.props;
+//     // if (el) {
+//     //   this.rootElement = this.getRootElement({ el });
+//     // }
+//     // if (routes) {
+//     //   this.setRoute();
+//     // }
+//   }
 
-  /* getRootElement({ el }) {
-    const div = document.createElement("div");
-    document.body.innerHTML = "";
-    div.setAttribute("id", el.replace("#", ""));
-    document.body.appendChild(div);
-    return document.body.querySelector(el);
-  } */
+//   // getRootElement({ el }) {
+//   //   const div = document.createElement("div");
+//   //   document.body.innerHTML = "";
+//   //   div.setAttribute("id", el.replace("#", ""));
+//   //   document.body.appendChild(div);
+//   //   return document.body.querySelector(el);
+//   // }
 
-  /* setRoute() {
-    const { routes, pathname } = this.props;
-    const component = require("../" + routes[pathname]).default;
-    this.render({
-      element: this.rootElement,
-      component
-    });
+//   // setRoute() {
+//   //   const { routes, pathname } = this.props;
+//   //   const component = require("../" + routes[pathname]).default;
+//   //   this.render({
+//   //     element: this.rootElement,
+//   //     component
+//   //   });
 
-    console.log(this.rootElement);
-  }
+//   //   console.log(this.rootElement);
+//   // }
 
-  render({ element, component }) {
-    element.innerHTML = new component(this.props).render();
-    return element.innerHTML;
-  }
- */
-  /*
-  getPathName() {
-    const { pathname } = this.props;
-    return pathname;
-  }
+//   // render({ element, component }) {
+//   //   element.innerHTML = new component(this.props).render();
+//   //   return element.innerHTML;
+//   // }
 
-  setHistory({ pathname }) {}
-  */
-  /* async getComponent({ component }) {
-    if (typeof component === 'string') {
-      component = await import(`../${component}`);
+//   // getPathName() {
+//   //   const { pathname } = this.props;
+//   //   return pathname;
+//   // }
 
-      component = component.default;
-    }
-    return component;
-  } */
-}
+//   // setHistory({ pathname }) {}
 
-export default Server;
+//   // async getComponent({ component }) {
+//   //   if (typeof component === 'string') {
+//   //     component = await import(`../${component}`);
+
+//   //     component = component.default;
+//   //   }
+//   //   return component;
+//   // }
+// }
+
+// export default Server;
